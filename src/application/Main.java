cadc1ac837ff5b637677f0b13b9111a204dc3476
victor <luanvitorlima2017@gmail.com>
2025-04-07 20:55:03 -0300

    modelo das perguntas

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,20 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the departament's name: ");
+        String departamentName = sc.nextLine();
+        System.out.print("Enter the worker data: ");
+        System.out.print("Name: ");
+        String nameWorker = sc.nextLine();
+        System.out.print("Level: ");
+        String WorkerLevel = sc.nextLine();
+        System.out.print("Base Salary: ");
+        double baseSalary = sc.nextDouble();
     }
 }
